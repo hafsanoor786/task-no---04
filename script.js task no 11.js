8fb7f067f09ae270d0bc62aebd21678aaa40c83c
hafsanoor786 <ansari366330@gmail.com>
2026-03-07 21:18:40 +0500

    task no 11

---
+++ script.js task no 11.js
@@ -0,0 +1,136 @@
+// javascript operators
+
+// arithmetic operators
+
+// arithmetic operators numbers par mathematical calculation krne ke liye use hote hain
+
+// for example:
+// addition + , subtraction - , multiplication * , division / , modulus % , increment ++ , decrement --
+
+// ADDITION
+
+
+let a= 2
+let b= 3
+let add= a+b;
+console.log (add);
+
+
+// SUBTRUCTION
+
+
+let c= 3
+let d= 1
+let sub= c-d
+console.log (sub);
+
+
+// MULTIPILICATION
+
+
+let e= 5
+let f= 3
+let mul= e*f
+console.log (mul);
+
+
+// DIVISION
+
+let i= 10
+let j= 2
+let div= i/j
+console.log (div);
+
+
+
+// INCREMENT(++)
+// VALUE ME SE 1 KO INCREASE MTLAD ADD KRTA HAI
+
+let h= 7
+h++
+console.log(h++)
+
+
+
+// DECREMENT (--)
+// VALUE ME SE 1 KO DECREASE KRTA HAI
+
+let g =5
+g--
+console .log (g--);
+
+
+
+
+
+
+
+// assigmnent operators
+// assignment operators kisi variable ko value assign krne ke liye use hote hain
+// mtlb hr operator ke sath (=) lagta hai
+
+
+// addition assignment (+=)
+let k= 5
+k += 3
+console.log (k);
+
+
+
+// modululos assignment
+let l= 4
+l %= 3
+console .log (l);
+
+
+
+
+
+
+
+// comparison operators
+// comparison operators do values ko compare krne ke liye use hote hain  aur result TRUE ya FALSE me dete hain
+// for example: equal to (==) , not equal to (!=) , greater than (>) , less than (<) , greater than or equal to (>=) , less than or equal to (<=)
+
+// equal to (==)
+let m= 5
+let n= 5
+console.log (m==n);
+
+
+
+
+
+// loggical operators
+// logical operators do ya zyada conditions ko combine krne ke liye use hote hain aur result TRUE ya FALSE me dete hain
+// for example: AND (&&) , OR (||) , NOT (!)
+
+
+
+
+// AND (&&)
+let o= 5
+let p= 10
+console.log (o>3 && p<15);
+
+
+
+
+
+// unary operators
+// unary operators ek hi operand par operate krte hain
+// for example: typeof , delete , void
+
+// typeof
+let q= "hello"
+console.log (typeof q);
+
+
+
+// ternary operator
+// ternary operator ek conditional operator hai jo 3 operands leta hai
+// for example: condition ? expression1 : expression2
+
+let r= 5
+let s= (r>3) ? "greater than 3" : "less than or equal to 3"
+console.log (s);
